8f51c86a6e21bc991c7e47a5a21ef33500dabd9c
AurelieG13 <108137928+AurelieG13@users.noreply.github.com>
2024-01-29 16:37:22 +0100

    contact entity

---
+++ src/main/java/com/aguichardon/springtest3/service/EmailService.java
@@ -0,0 +1,24 @@
+package com.aguichardon.springtest3.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender emailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            emailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
